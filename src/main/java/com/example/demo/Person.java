e80341d2b5bf8f03f7003115889dcc7270fed05a
michael-doll <MWDoll88@gmail.com>
2019-12-11 13:55:40 -0500

    project setup

---
+++ src/main/java/com/example/demo/Person.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+public class Person {
+    private Long id; //final 
+    private String name;
+
+    public Person(){}
+    public Person(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
