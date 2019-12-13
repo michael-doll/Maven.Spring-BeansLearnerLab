04ddc5b7e8dcb406d9f5764c8e5b4d5ec4803739
michael-doll <MWDoll88@gmail.com>
2019-12-12 19:21:08 -0500

    save work, 6.0, add tests

---
+++ src/main/java/com/example/demo/People.java
@@ -0,0 +1,68 @@
+package com.example.demo;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+public abstract class People<PersonType extends Person> implements Iterable<PersonType> {
+    List<PersonType> personList;
+    public People(List<PersonType> personList){
+           this.personList = personList;
+    }
+    public People(PersonType... personType){
+        this.personList.addAll(Arrays.asList(personType));
+    }
+
+    public void add (PersonType personType){
+        this.personList.add(personType);
+    }
+
+    public void remove (PersonType personType){
+        if(personList.contains(personType))
+        this.personList.remove(personType);
+    }
+
+    public int size (){
+        return this.personList.size();
+    }
+
+    public void clear (){
+        this.personList.clear();
+    }
+
+    public void addAll(Iterable<PersonType> iter){
+        for(PersonType p : iter){
+            personList.add(p);
+        }
+    }
+
+    public PersonType findById(Long Id){
+        for(PersonType p : personList){
+            if(p.getId().equals(Id)){
+                return p;
+            }
+        }
+        return null; 
+    }
+
+    public List findAll(){
+        return this.personList;
+    }
+
+
+    //Create an abstract People class
+    //
+    //    The class signature should be parameterized with PersonType such that PersonType is a sub class of Person.
+    //    The class should implement Iterable of type PersonType.
+    //    Upon construction People should consume a List of PersonType and set it to a respective personList field.
+    //    The class should define a constructor which consumes a variable number of PersonType objects and sets the personList field respectively.
+    //    The class should define a method named add which adds a PersonType to the personList.
+    //    The class should define a method named remove which removes a PersonType from the personList.
+    //    The class should define a method named size which returns the size of personList.
+    //    The class should define a method named clear which clears our personList field.
+    //    The class should define a method named addAll which adds an Iterable of PersonType objects to the composite personList.
+    //        This method will NOT take an argument of Collection<PersonType>
+    //    The class should define a method named findById which makes use of a long id parameter to return a PersonType object with the respective id field.
+    //    The class should define a method named findAll which returns the composite personList.
+}
---
+++ src/main/java/com/example/demo/Students.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class Students extends People<Student> {
+
+}
