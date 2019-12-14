314fc75b97eda15cc7c012aa5583209a45fab80b
michael-doll <MWDoll88@gmail.com>
2019-12-14 13:51:27 -0500

    save work, 11.3

--- src/test/java/com/example/demo/TestClassroomConfig.java
+++ src/test/java/com/example/demo/TestClassroomConfig.java
@@ -21,4 +21,6 @@ public class TestClassroomConfig {
     Classroom previousCohort;
 
 
+
+
 }
--- src/test/java/com/example/demo/TestInstructorConfig.java
+++ src/test/java/com/example/demo/TestInstructorConfig.java
@@ -1,4 +1,49 @@
 package com.example.demo;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestInstructorConfig {
+
+    @Autowired
+    @Qualifier("tcUsaInstructors")
+    Students tcUsaInstructors;
+
+    @Autowired
+    @Qualifier("tcUkInstructors")
+    Students tcUkInstructors;
+
+    @Autowired
+    @Qualifier("zipCodeWilmington Instructors")
+    Students instructors;
+
+    @Test
+    public void testTcUsaInstructors(){
+        InstructorsConfig instructorsConfig = new InstructorsConfig();
+        String expected = "Al Kooper";
+        String actual = instructorsConfig.tcUsaInstructors().findById(1L).getName() ;
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testTcUkInstructors(){
+        InstructorsConfig instructorsConfig = new InstructorsConfig();
+        String expected = "Ginger Baker";
+        String actual = instructorsConfig.tcUkInstructors().findById(1L).getName() ;
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testInstructors(){
+        InstructorsConfig instructorsConfig = new InstructorsConfig();
+        String expected = "Jorma";
+        String actual = instructorsConfig.instructors().findById(1L).getName() ;
+        Assert.assertEquals(expected,actual);
+    }
+
 }
